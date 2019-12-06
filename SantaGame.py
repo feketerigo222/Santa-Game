0f9b3c5dfd773ac9de6a1af06288f13330e6bda0
feketerigo222 <biss1280449@gn.iwasaki.ac.jp>
2019-12-06 12:06:51 +0900

    ひとまず土台

--- SantaGame.py
+++ SantaGame.py
@@ -1,5 +1,17 @@
 import tkinter
+import SantaGameFight
 
+
+# サンタの位置
+santa_x = 1
+santa_y = 0
+santa = SantaGameFight.Santa()
+
+# 初期フラグ
+flag_ax = False
+flag_money = False
+flag_toy = False
+flag_emergency = False
 # マップ描画
 
 
@@ -7,12 +19,170 @@ def draw_map():
     for y in range(0, MAX_HEIGHT):
         for x in range(0, MAX_WIDTH):
             p = map_data[y][x]
-            if p >= 5:
-                p = 5
-            canvas.create_image(x * 62 + 31, y * 62 + 31, image=images[p])
+            if p >= 8:
+                p = 8
+            canvas.create_image(x * 64 + 31, y * 64 + 31, image=mapImages[p])
 
     # サンタ表示
-    canvas.create_image(santa_x * 62 + 31, santa_y * 62 +
-                        31, image=images[4], tag="santa")
+    canvas.create_image(santa_x * 64 + 31, santa_y * 64 +
+                        31, image=santaImages[0], tag="santa")
 
 # 移動先のチェック
+
+
+def check_move(x, y):
+    global santa_x, santa_y, flag_ax, flag_money, flag_toy, flag_ax
+    if x >= 0 and x < MAX_WIDTH and y >= 0 and y < MAX_HEIGHT:
+        p = map_data[y][x]
+        if p == 2:
+            return
+        elif p == 3:
+            if flag_toy == False:
+                # おもちゃがないのコメント
+                return
+            elif flag_ax == False:
+                # 家に入れないのコメント
+                return
+            else:
+                ending()
+                return
+        elif p == 4:
+            flag_money = True
+            map_data[y][x] = 0
+            canvas.delete("all")
+            draw_map()
+        elif p == 5:
+            flag_ax = True
+            map_data[y][x] = 0
+            canvas.delete("all")
+            draw_map()
+        elif p == 6:
+            if flag_money == True:
+                flag_toy = True
+                # おもちゃを買ったのコメント
+                map_data[y][x] = 0
+                canvas.delete("all")
+                draw_map()
+            elif flag_ax == True:
+                flag_toy = True
+                flag_emergency = True
+                # おもちゃを強盗したのコメント
+                map_data[y][x] = 1
+                canvas.delete("all")
+                draw_map()
+            else:
+                # お金が無いのコメント
+                return
+
+        elif p >= 8:
+            fightmanager.fight_start(map_data, x, y, santa)
+        santa_x = x
+        santa_y = y
+        canvas.delete("all")
+        draw_map()
+
+# 上ボタンが押された
+
+
+def click_button_up():
+    check_move(santa_x, santa_y-1)
+
+# 下ボタンが押された
+
+
+def click_button_down():
+    check_move(santa_x, santa_y+1)
+
+# 左ボタンが押された
+
+
+def click_button_left():
+    check_move(santa_x-1, santa_y)
+
+# 右ボタンが押された
+
+
+def click_button_right():
+    check_move(santa_x+1, santa_y)
+
+# エンディング表示
+
+
+def ending():
+    canvas.delete("all")
+    canvas.create_rectangle(0, 0, 620, 434, fill="black")
+    canvas.create_text(300, 200, fill="white", font=(
+        "MS ゴシック", 15), text="""ゴールおめでとう。
+        
+        だが、君の戦いはまだ始まったばかりだ。
+        
+        
+                             …つづく？""")
+
+    # ボタンを無効か
+    button_up["state"] = "disabled"
+    button_down["state"] = "disabled"
+    button_left["state"] = "disabled"
+    button_right["state"] = "disabled"
+
+
+# ウィンドウ作成
+root = tkinter.Tk()
+root.title("Santa Game")
+root.minsize(840, 454)
+root.option_add("*font", ["メイリオ", 14])
+
+# キャンバス作成
+canvas = tkinter.Canvas(width=620, height=434)
+canvas.place(x=10, y=10)
+canvas.create_rectangle(0, 0, 620, 434, fill="gray")
+
+# ボタン配置
+button_up = tkinter.Button(text="↑")
+button_up.place(x=720, y=150)
+button_up["command"] = click_button_up
+button_down = tkinter.Button(text="↓")
+button_down.place(x=720, y=210)
+button_down["command"] = click_button_down
+button_left = tkinter.Button(text="←")
+button_left.place(x=660, y=180)
+button_left["command"] = click_button_left
+button_right = tkinter.Button(text="→")
+button_right.place(x=780, y=180)
+button_right["command"] = click_button_right
+
+# 画像データ読み込み
+# サンタ画像
+santaImages = [tkinter.PhotoImage(file="img/santa/front.gif"),
+               tkinter.PhotoImage(file="img/santa/right.gif"),
+               tkinter.PhotoImage(file="img/santa/left.gif"),
+               tkinter.PhotoImage(file="img/santa/back.gif"), ]
+
+mapImages = [tkinter.PhotoImage(file="img/map/map.png"),
+             tkinter.PhotoImage(file="img/map/blood.png"),
+             tkinter.PhotoImage(file="img/map/tree.png"),
+             tkinter.PhotoImage(file="img/map/house.png"),
+             tkinter.PhotoImage(file="img/map/money.png"),
+             tkinter.PhotoImage(file="img/map/ax.png"),
+             tkinter.PhotoImage(file="img/map/toy.png"),
+             tkinter.PhotoImage(file="img/map/blood2.png"),
+             tkinter.PhotoImage(file="img/map/enemy.png")]
+
+# マップデータ
+MAX_WIDTH = 10
+MAX_HEIGHT = 7
+map_data = [[2, 0, 2, 2, 2, 2, 2, 2, 2, 2],
+            [2, 0, 0, 0, 3, 0, 0, 1, 4, 2],
+            [2, 0, 4, 2, 2, 1, 0, 1, 6, 2],
+            [2, 0, 0, 5, 0, 0, 0, 1, 0, 2],
+            [2, 0, 0, 0, 0, 1, 1, 1, 8, 2],
+            [2, 0, 8, 0, 0, 0, 5, 0, 0, 2],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]]
+
+
+# 戦闘画面の準備
+fightmanager = SantaGameFight.FightManager()
+
+draw_map()
+
+root.mainloop()
---
+++ SantaGameFight.py
@@ -0,0 +1,216 @@
+import tkinter
+import random
+import time
+
+
+class FightManager:
+    # コンストラクタ
+    def __init__(self):
+        self.dialog = tkinter.Frame(width=820, height=434)
+        self.dialog.place(x=10, y=10)
+        self.canvas = tkinter.Canvas(self.dialog, width=820, height=434)
+        self.canvas.place(x=0, y=0)
+        self.canvas.create_rectangle(0, 0, 620, 434, fill="black")
+        # ボタン作成
+        self.fbutton = tkinter.Button(self.dialog, text="攻撃")
+        self.fbutton.place(x=180, y=340)
+        self.fbutton["command"] = self.click_fight
+        self.rbutton = tkinter.Button(self.dialog, text="力をためる")
+        self.rbutton.place(x=320, y=340)
+        self.rbutton["command"] = self.click_reserve
+        # # 非表示
+        self.dialog.place_forget()
+        # 画像の読み込み
+        self.images = [tkinter.PhotoImage(file="img/map/police.png"),
+                       tkinter.PhotoImage(file="img/map/yakuza.png"),
+                       tkinter.PhotoImage(file="img/map/police_emergency.png")]
+        self.canvas.create_image(180, 160, image=self.images[0])
+        # ラベルを配置
+        self.label = tkinter.Label(
+            self.dialog, text="ラベル", fg="white", bg="black", justify="left")
+        self.label.place(x=360, y=10)
+
+    # 戦闘開始
+
+    def fight_start(self, mapdata, x, y, santa):
+        self.dialog.place(x=10, y=10)
+        self.map_data = mapdata
+        self.santa_x = x
+        self.santa_y = y
+        self.santa = santa
+        # 敵の画像を表示
+        p = self.map_data[y][x]
+        self.canvas.delete("all")
+        self.canvas.create_rectangle(0, 0, 620, 434, fill="black")
+        self.canvas.create_image(180, 160, image=self.images[p-8])
+        self.label["text"] = ""
+        # 敵ののオブジェクトを作成
+        if p == 8:
+            self.enemy = Police()
+        elif p == 9:
+            self.enemy = Yakuza()
+        self.label["text"] = self.enemy.name + "が現れた"
+
+    # 攻撃ボタン
+
+    def click_fight(self):
+        self.fbutton["state"] = "disabled"
+        self.rbutton["state"] = "disabled"
+        self.do_turn(self.santa.get_atk())
+
+    # 力をためるボタン
+
+    def click_reserve(self):
+        self.fbutton["state"] = "disabled"
+        self.rbutton["state"] = "disabled"
+        self.santa.reserve()
+        self.do_turn(-1)
+
+    # 戦闘処理
+    def do_turn(self, santa_atk):
+        # 主人公のターン
+        enemy_dfs = self.enemy.get_dfs()
+        if santa_atk < 0:
+            labeltext = "サンタは力をためた"
+        else:
+            labeltext = "サンタは攻撃した"
+            self.label["text"] = labeltext
+            self.dialog.update()
+            # サンタの攻撃の結果表示
+            time.sleep(2)  # 2秒待ち
+            dmg = santa_atk - enemy_dfs
+            self.enemy.culc_hp(santa_atk, enemy_dfs)
+            if dmg <= 0:
+                labeltext = labeltext + "\n防がれた"
+            else:
+                labeltext = labeltext + "\n" + str(dmg) + \
+                    "のダメージを与えた"
+        # ラベル更新、残り体力表示
+        self.label["text"] = labeltext
+        self.dialog.update()
+        time.sleep(2)  # 2秒待ち
+        labeltext = labeltext + \
+            "\n敵のの残り体力は" + str(self.enemy.hp)
+        self.label["text"] = labeltext
+        self.dialog.update()
+        if self.enemy.hp < 1:
+            time.sleep(2)  # 2秒待ち
+            self.fbutton["state"] = "normal"
+            self.rbutton["state"] = "normal"
+            self.fight_win()
+            return
+        # 敵のターン
+        time.sleep(2)  # 2秒待ち
+        santa_dfs = self.santa.get_dfs()
+        if random.random() < 0.2:
+            labeltext = labeltext + "\n\n敵はは力をためた"
+            self.enemy.reserve()
+        else:
+            labeltext = labeltext + "\n\n敵の攻撃"
+            self.label["text"] = labeltext
+            self.dialog.update()
+            if self.santa.hp < 1:
+                time.sleep(2)  # 2秒待ち
+                self.fight_lose()
+            else:
+                #　ボタンを有効化して次のターンへ
+                self.fbutton["state"] = "normal"
+                self.rbutton["state"] = "normal"
+            # 敵の攻撃の結果表示
+            time.sleep(2)  # 2秒待ち
+            enemy_atk = self.enemy.get_atk()
+            dmg = enemy_atk - santa_dfs
+            self.santa.culc_hp(enemy_atk, santa_dfs)
+            if dmg <= 0:
+                labeltext = labeltext + "\n防いだ"
+            else:
+                labeltext = labeltext + "\n" + str(dmg) + \
+                    "のダメージを受けた"
+        # ラベル更新、残り体力表示
+        self.label["text"] = labeltext
+        self.dialog.update()
+        time.sleep(2)  # 2秒待ち
+        labeltext = labeltext + \
+            "\nサンタの残り体力は" + str(self.santa.hp)
+        self.label["text"] = labeltext
+        self.dialog.update()
+    # 勝利
+
+    def fight_win(self):
+        self.map_data[self.santa_y][self.santa_x] = 0
+        # 不審者フラグ立ってれば血痕に変える
+        self.dialog.place_forget()
+
+    # 敗北
+    def fight_lose(self):
+        canvas = tkinter.Canvas(self.dialog, width=820, height=434)
+        canvas.place(x=0, y=0)
+        canvas.create_rectangle(0, 0, 620, 434, fill="red")
+        canvas.create_text(300, 200, fill="white", font=("MS ゴシック", 15), text="""勇者は負けてしまった
+        最初からやり直してくれたまえ""")
+
+# キャラクターの親クラス
+
+
+class Character:
+    # コンストラクタ
+    def __new__(cls):
+        obj = super().__new__(cls)
+        obj.rsv = 1
+        return obj
+    # 力をためる
+
+    def reserve(self):
+        self.rsv = self.rsv + 1
+    # 攻撃力を求める
+
+    def get_atk(self):
+        r = self.rsv
+        self.rsv = 1
+        return random.randint(1, self.atk * r)
+    # 防御力を求める
+
+    def get_dfs(self):
+        return random.randint(0, self.dfs)
+    # 体力計算
+
+    def culc_hp(self, atk, dfs):
+        dmg = atk - dfs
+        # ダメージ無し
+        if dmg < 1:
+            return self.hp
+        # 体力を減らす
+        self.hp = self.hp - dmg
+        if self.hp < 1:
+            self.hp = 0
+        return self.hp
+
+# サンタクラス
+
+
+class Santa(Character):
+    def __init__(self):
+        self.name = "サンタ"
+        self.hp = 30
+        self.atk = 15
+        self.dfs = 10
+
+# 警官
+
+
+class Police(Character):
+    def __init__(self):
+        self.name = "警察官"
+        self.hp = 20
+        self.atk = 15
+        self.dfs = 8
+
+# ヤクザ
+
+
+class Yakuza(Character):
+    def __init__(self):
+        self.name = "ヤクザ"
+        self.hp = 10
+        self.atk = 8
+        self.dfs = 5
